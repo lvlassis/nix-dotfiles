27dbe1d9fe9b0281cc762d7bd8f7650ca95e6420
Lucas Vinícius de L. Assis <lvlassis.2@gmail.com>
2025-12-29 09:11:52 +0000

    refact: gerencia variáveis de ambiente com o nix

--- common/home-manager/hyprland/env_vars.nix
+++ common/home-manager/hyprland/env_vars.nix
@@ -1,24 +1,8 @@
 { config, pkgs, ... }:
 
 {
+    # Variáveis de ambiente foram migradas para sessionVariables.nix
     wayland.windowManager.hyprland.settings = {
-        
-        env = [
-            "XCURSOR_SIZE,16"
-            "XDG_CURRENT_DESKTOP, Hyprland"
-            "XDG_SESSION_TYPE, wayland"
-            "XDG_SESSION_DESKTOP, Hyprland"
-            "QT_AUTO_SCREEN_SCALE_FACTOR, 1"
-            "QT_WAYLAND_DISABLE_WINDOWDECORATION, 1"
-            "QT_QPA_PLATFORMTHEME, qt5ct"
-            "GDK_BACKEND, wayland,x11"
-            "QT_QPA_PLATFORM, xcb"
-            "SDL_VIDEODRIVER, wayland"
-            "CLUTTER_BACKEND, wayland"
-            "WLR_RENDERER, vulkan"
-            "EDITOR, nvim"
-            "VISUAL, nvim"
-        ];
 
     };
 }
