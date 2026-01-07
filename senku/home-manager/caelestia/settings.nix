73657b59083d5b4c586b4d98aa4498f60b719fe6
Sevenings <lvlassis.2@gmail.com>
2026-01-07 11:22:33 -0300

    add senku caelestia config

---
+++ senku/home-manager/caelestia/settings.nix
@@ -0,0 +1,18 @@
+{ ... }:
+
+{
+  programs.caelestia.settings = {
+    background = {
+      desktopClock.enabled = true;
+    };
+
+    general.idle = {
+      timeouts = [
+        { "timeout": 180, "idleAction": "lock" }
+        { "timeout": 300, "idleAction": "dpms off", "returnAction": "dpms on" }
+        { "timeout": 600, "idleAction": ["notify-send" "Hibernando"] }
+      ]
+    };
+
+  };
+}
